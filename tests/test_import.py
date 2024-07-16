0f4f7e8d1d19dff54485051dfd29f6f30e7c1b9b
sanjaypranav <sanjaypriya195@gmail.com>
2024-07-17 00:46:29 +0530

    chore: Update pytest configuration to include "src" in pythonpaths

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../src')))
+
+
 def test_import():
     from sayvai_rag import __version__, search_index
 
